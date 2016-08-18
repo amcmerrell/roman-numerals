0ea57c34cef5c761547be0543108331742a0049e
Andrew Merrell and Josh Huffman <josh@>
2016-08-18 11:25:56 -0700

    create roman numerals array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 // Business Logic
+var romanNums = ["M","D", "C", "L", "X", "V", "I"];
+
 function isValid(input) {
   if (isNaN(input) === true || input <= 0 || input >= 4000 || input !== Math.floor(input)) {
     alert("Please enter a whole number between 1 and 3,999");
@@ -13,11 +15,23 @@ function separator(number) {
   numberArray.forEach(function(element, index){
     numberArray[index] = parseInt(element);
   });
+
   for(;numberArray.length < 4;){
     numberArray.unshift(0);
   }
-
   return numberArray;
+}
+
+function fiver(array) {
+  for (i=1; i<array.length; i+=2) {
+    if (array[i] >= 5) {
+      array[i] = array[i] - 5;
+      array.splice(i, 0, 1);
+    } else {
+      array.splice(i, 0, 0);
+    }
+  }
+  return array;
 }
 
 // UI Logic
